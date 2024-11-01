028d08f3d60b46360e1bbb1f9af6897162ad7612
Aosaf098 <aosafibad10@gmail.com>
2024-11-01 22:18:00 +0600

    Home Page

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Banner from "./components/Banner"
 import Books from "./components/Books"
+import Footer from "./components/Footer"
 
 const Root = () => {
   return (
@@ -9,6 +10,7 @@ const Root = () => {
         <Navbar />
         <Banner />
         <Books />
+        <Footer />
      </div>
     </>
   )
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
 
   return (
     <>
-        <div className="mx-auto rounded-xl shadow-lg lg:w-[80%] mt-28 lg:mt-44 pt-24 lg:py-40 lg:px-10 flex flex-col lg:flex-row justify-around lg:items-center lg:gap-10 gap-24 bg-opacity-50">
+        <div className="mx-auto rounded-xl shadow-lg lg:w-[80%] mt-28 lg:mt-44 pt-24 lg:py-40 lg:px-10 flex flex-col lg:flex-row justify-around lg:items-center lg:gap-10 gap-24 bg-opacity-50 bg-base-300">
             <div className='space-y-14 lg:block flex flex-col items-center'>
                <h1 className='font-sora font-bold text-6xl text-center lg:text-left'>Books to Freshen Up your Bookshelf</h1> 
                <button className='text-center font-sora font-bold text-xl p-2 lg:p-4 bg-third rounded-lg hover:bg-secon'>View The List</button>
